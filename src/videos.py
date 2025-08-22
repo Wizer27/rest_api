44c4033898f19775d6f1f1ae3d6bcdeb01ec9445
Wizer27 <svd2yar@gmail.com>
2025-08-22 12:13:13 +0300

    Coding

--- src/videos.py
+++ src/videos.py
@@ -21,10 +21,46 @@ def contains_username(username:str) -> bool:
 
     return False        
 
-def base_videos_data(username:str):
+def base_videos_data(username:str) -> bool:
     with open("data/videos.json","r") as file:
         data = json.load(file)
 
+    if not contains_username(username):
+        data.append({
+            "username":username,
+            "videos":{}
+        }) 
+        with open("data/videos.json","w") as file:
+            json.dump(data,file,indent=2)
+            return True
+    else:
+        print("User already exists")
+        return False
+        
 
+
+class Username(BaseModel):
+    username :str
+
+
+@app.post("/load/default/videos")
+async def load_def(request:Username):
+    base_videos_data(request.username)
+
+
+class JsonDataUser(BaseModel):
+    username:str
+    code:str
+    title:str
+
+@app.post("/write/video/user")
+
+async def write_video_to_user(request:JsonDataUser):
+    with open("data/videos.json","r") as file:
+        data = json.load(file)
+
+    for user in data:
+        if user["username"]  == request.username:
+            pass    
 
 
