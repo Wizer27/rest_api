c956a35a3eb4c92f064b8e72299e224fcc1812a3
Wizer27 <svd2yar@gmail.com>
2025-08-27 17:06:41 +0300

    Rqiester enpoint

--- src/ai_api.py
+++ src/ai_api.py
@@ -1,8 +1,43 @@
 from fastapi import FastAPI,HTTPException
 import json
+from pydantic import BaseModel
+import hashlib
 
 app = FastAPI()
 
 @app.get("/")
 async def main():
     return "AI urist API"
+
+def has_key(key:str,data) -> bool:
+    return key in data
+
+def hash_username(username:str):
+    byt = username.encode("utf-8")
+    hashed = hashlib.sha256(byt).hexdigest()
+    return hashed
+
+
+class UserReg(BaseModel):
+    username:str
+    hash_password:str
+
+
+@app.get("/user/register")
+async def requester_(request:UserReg):
+    if request.username == "" or (not request.username):
+        raise HTTPException(status_code=400,detail="Username is not valid")
+    with open("/Users/ivan/rest_api/data/users.json","r") as file:
+        data = json.load(file)
+
+    if has_key(request.username,data) or has_key(hash_username(request.username),data):
+        raise HTTPException(status_code=400,detail="User already exists")
+    else:
+        data[hash_username(request.username)] = request.hash_password
+
+
+    with open("/Users/ivan/rest_api/data/users.json","r") as file:
+        json.dump(data,file,indent=2)    
+
+        
+    
